e0fe64fa790ae3275cdd73b5a786b3b631452614
Divyansh Yadav <divyanshyadav20@gmail.com>
2021-09-22 01:26:13 +0530

    Now mobile responsive

--- index.js
+++ index.js
@@ -3,23 +3,3 @@ window.addEventListener("scroll", function () {
   const windowPosition = window.scrollY > 0;
   header.classList.toggle("scrolling-active", windowPosition);
 });
-
-// const body = document.body,
-//   scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-//   height = scrollWrap.getBoundingClientRect().height - 1,
-//   speed = 0.04;
-
-// var offset = 0;
-
-// body.style.height = Math.floor(height) + "px";
-
-// function smoothScroll() {
-//   offset += (window.pageYOffset - offset) * speed;
-
-//   var scroll = `translateY(-${offset}px) translateZ(0)`;
-//   scrollWrap.style.transform = scroll;
-
-//   callScroll = requestAnimationFrame(smoothScroll);
-// }
-
-// smoothScroll();
